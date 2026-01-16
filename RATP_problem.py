ab78d8e9f903127c53aaae8aaa204f500e86244e
thomaspogu <thomas.pogu@gmail.com>
2026-01-16 15:19:58 +0100

    update_ratp

--- RATP_problem.py
+++ RATP_problem.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import problem_resolution as pr
 
 data = pd.read_csv('dataset/RATP_csv.csv',sep=';', encoding='latin1')
 print(data.head())
@@ -20,5 +21,44 @@ for col, weight in zip(numeric_columns, weights):
 
 # 5. Affichage et sauvegarde
 score_final=data[['Metro station', 'Score_Final']].sort_values(by='Score_Final', ascending=False)
+score_final_all = data.sort_values(by='Score_Final', ascending=False)
+score_final_all.to_csv('dataset/RATP_score_final.csv', index=False, sep=';')
 
-print(score_final)
+
+
+# 2. Liste des colonnes de critères (noms originaux)
+criteria_cols = [
+    'peak-entering-passengers/h', 
+    'peak-passing-passengers/h', 
+    'off-peak-entering-passengers/h', 
+    'off-peak-passing-passengers/h', 
+    'strategic priority [0,10]', 
+    'Station degradation level ([0,20]  scale)', 
+    'connectivity index [0,100]'
+]
+
+# 3. Extraction de la première ligne comme référence (x)
+first_row = score_final_all.iloc[0]
+
+# 4. Construction du dictionnaire des différences (x - y)
+diff_dict = {}
+
+for i in range(1, len(score_final_all)):
+    current_row = score_final_all.iloc[i]
+    station_name = current_row['Metro station']
+    
+    # Calcul des différences : Ligne_1 - Ligne_i
+    row_diffs = {}
+    for col in criteria_cols:
+        val_diff = first_row[col] - current_row[col]
+        # On force en float pour la compatibilité JSON et on arrondit
+        row_diffs[col] = float(round(val_diff, 3))
+    
+    diff_dict[station_name] = row_diffs
+
+# 5. Affichage d'un exemple pour vérifier
+#print(diff_dict)
+
+
+pros,cons,neutral = pr.preprocess_data(diff_dict["La Motte Picquet-Grenelle (Ligne 10)"])
+print(pr.m_to_one_or_one_to_m_tradeoffs(diff_dict["La Motte Picquet-Grenelle (Ligne 10)"], pros, cons))
